5831cb42706c174c8f12f57cb5080a9b7a58d225
unknown <renancavalcantercb@protonmail.com>
2023-02-11 19:48:58 -0300

    changed template for profile page

---
+++ static/js/profile.js
@@ -0,0 +1,23 @@
+function parseJWT(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(window.atob(base64));
+}
+
+function getStudentData() {
+    const jwt = localStorage.getItem('jwt');
+    const student = parseJWT(jwt);
+    $.getJSON('https://gepac.netlify.app/subscribed/admin/' + student.id + '/view', function (data) {
+        console.log(data);
+    });
+}
+
+
+function disabledCPFandCourse() {
+    var cpf = document.getElementById("cpf");
+    var course = document.getElementById("course");
+    cpf.disabled = true;
+    course.disabled = true;
+    cpf.style.backgroundColor = "#262626";
+    course.style.backgroundColor = "#262626";
+}
